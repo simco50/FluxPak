7d3e18d07b37a81124dd6c3f922f014073b8a958
Simon Coenen - Gobo <sim2.coenen@gmail.com>
2018-01-23 13:34:06 +0000

    Better command line argument parsing

--- main.cpp
+++ main.cpp
@@ -1,69 +1,38 @@
 #include "FluxPak.h"
 #include <iostream>
 #include <string>
+#define NOMINMAX
 #include <windows.h>
-#include <map>
+#include "cxxopts.hpp"
 
 using namespace std;
 
-map<string, string> ParseArguments(int argc, char *argv[])
-{
-	map<string, string> arguments;
-
-	string currentCommand = "";
-	bool command = false;
-	for (int i = 1; i < argc; i++)
-	{
-		if (command)
-		{
-			arguments[currentCommand] = argv[i];
-			command = false;
-		}
-		else
-		{
-			if (argv[i][0] == '-')
-			{
-				command = true;
-				string cmd = argv[i];
-				currentCommand = cmd.substr(1);
-			}
-			else
-			{
-				command = false;
-				arguments[argv[i]] = argv[i];
-			}
-		}
-	}
-	return arguments;
-}
-
-#define REQUIRE(argumentMap, argument, description) \
-if(argumentMap.find(argument) == argumentMap.end()) \
-{ \
-cout << "Missing argument '-" << argument << "'. The script requires a " << description << "." << endl; \
-cout << "Usage: " << endl; \
-cout << "\t FluxPak.exe -r [responsefile] -p [pakfile filepath] -b [base virtual path]" << endl; \
-return -1; \
-}
-
 int main(int argc, char *argv[])
 {
-	auto argumentMap = ParseArguments(argc, argv);
+	cxxopts::Options options(argv[0], "-r [ResponseFile] -p [Pakfile Name] -b [Virtual directory]");
+	options.add_options()
+		("r, responsefile", "The input response file", cxxopts::value<std::string>())
+		("p, pakfile", "The output pak file", cxxopts::value<std::string>())
+		("b, virtualdir", "The virtual base directory", cxxopts::value<string>());
 
-	REQUIRE(argumentMap, "r", "ResponseFile path");
-	REQUIRE(argumentMap, "p", "Pakfile name");
-	REQUIRE(argumentMap, "b", "Virtual directory");
-	string responseFilePath = argumentMap["r"];
-	string pakFileName = argumentMap["p"];
-	string baseDirectoryPath = argumentMap["b"];
+	cxxopts::ParseResult result = options.parse(argc, argv);
+
+	if (result.count("r") != 1 || result.count("p") != 1 || result.count("b") != 1)
+	{
+		cout << options.help() << endl;
+		return 1;
+	}
+	string responseFilePath = result["i"].as<std::string>();
+	string pakFileName = result["o"].as<std::string>();
+	string baseDirectoryPath = result["e"].as<string>();
 
 	LARGE_INTEGER start, end, freq;
 	QueryPerformanceFrequency(&freq);
 	QueryPerformanceCounter(&start);
 
 	cout << "Creating pak file..." << endl;
-	FluxPak::PAK_RESULT result = FluxPak::CreatePakFile(responseFilePath, pakFileName, baseDirectoryPath, FluxPak::PAK_COMPRESSION_QUALITY::FAST);
-	cout << FluxPak::GetError(result) << endl;
+	FluxPak::PAK_RESULT pakResult = FluxPak::CreatePakFile(responseFilePath, pakFileName, baseDirectoryPath, FluxPak::PAK_COMPRESSION_QUALITY::FAST);
+	cout << FluxPak::GetError(pakResult) << endl;
 
 	QueryPerformanceCounter(&end);
 	cout << "Pak file created in " << (double)(end.QuadPart - start.QuadPart) / freq.QuadPart << " second(s)"<< endl;
